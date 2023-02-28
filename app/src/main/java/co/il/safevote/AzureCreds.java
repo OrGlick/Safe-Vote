70f3df467cf77c3cb6f058cc61c74b2dcc8d65a9
Or Glick <96243154+OrGlick@users.noreply.github.com>
2023-02-28 23:12:54 +0200

    Removal of unused imports

--- app/src/main/java/co/il/safevote/AzureCreds.java
+++ app/src/main/java/co/il/safevote/AzureCreds.java
@@ -1,13 +1,5 @@
 package co.il.safevote;
 
-import androidx.annotation.NonNull;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
 public class AzureCreds
 {
     private String endPoint;
